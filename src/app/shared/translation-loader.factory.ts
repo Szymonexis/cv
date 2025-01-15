e9eb72470d1a0988ab576bce2e9d4dbe04afa6a8
Szymon Kaszuba-Galka <szymonkaszuba3@wp.pl>
2025-01-15 22:19:08 +0100

    update

--- src/app/shared/translation-loader.factory.ts
+++
@@ -1,7 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-// Factory function to load translations
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
-}
